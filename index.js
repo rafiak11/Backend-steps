1161b2dbb22a13c8e4f3bc0aef22777395ab24bc
Yusra <yusra13579khan@gmail.com>
2024-10-16 14:28:30 +0530

    changes the path

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.use(express.static(path.join(__dirname, '../client/build')));
 
 // Handles requests for the React app
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '..client/build', 'index.html'));
 });
 
 
